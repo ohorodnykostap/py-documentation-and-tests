081ee533a14898540ddb7912a7d87f47a6d3410a
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2026-01-08 22:53:30 +0200

    Fixed bug

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-from user.views import CreateUserView, ManageUserView
+from rest_framework_simplejwt.views import TokenRefreshView
+from user.views import CreateUserView, ManageUserView, CreateTokenView
 
 
 app_name = "user"
@@ -11,7 +8,7 @@ app_name = "user"
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
 
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/", CreateTokenView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 
     path("me/", ManageUserView.as_view(), name="me"),
